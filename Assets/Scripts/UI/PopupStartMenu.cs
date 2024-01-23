5fa7a305e5707575a7a0a8ef5507d540ac083bb3
yomgoun <ygy718500@gmail.com>
2024-01-24 02:58:30 +0900

    애니메이션,버튼

---
+++ Assets/Scripts/UI/PopupStartMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupStartMenu : MonoBehaviour
+{
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Text playerName;
+
+    public void OnClickJoin()
+    {
+        if(2 < inputField.text.Length && inputField.text.Length < 10)
+        {
+            return;
+        }
+
+        playerName.text = inputField.text;
+
+        Destroy(gameObject);
+    }
+}
